1eb8a266a42d2a99cf5ac2553f2a92a26fb88dbc
MuhammadRizkyRamadhan24 <zidan.muh69@gmail.com>
2021-07-06 23:42:38 +0700

    add component subtitle

---
+++ src/component/subtitle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const subtitle = props => {
+  return <Text style={styles.text}>{props.title}</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#6A4029',
+    fontFamily: 'Poppins-Bold',
+    fontSize: 18,
+  },
+});
+
+export default subtitle;
